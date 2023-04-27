cdf28717aac21dcf09cbd635cab6be96d88aa1f5
Dima <dimadeloseros1@gmail.com>
2023-04-27 20:23:03 +0100

    Chart

--- js/index.js
+++ js/index.js
@@ -28,12 +28,14 @@ const form = document.querySelector('#contact')
 
 
 function formes(event) {
-    const name = document.querySelector('#name').value
+    const name = document.querySelector('#name').value   
     const email = document.querySelector('#email').value
    
     if(name === '' || email === ''){
         alert('Please fill out the form!')
-        event.preventDefault(); // prevent form submission
+        event.preventDefault(); 
     }
 }
 form.addEventListener('submit', formes)
+
+
